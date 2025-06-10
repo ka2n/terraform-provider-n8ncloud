a35648f42364fa5216577ab0f573cafa661e0fe7
Katsuma Ito <katsumai@gmail.com>
2025-06-10 20:11:03 +0900

    feat: initial commit based on https://github.com/hashicorp/terraform-provider-scaffolding-framework

---
+++ internal/client/models.go
@@ -0,0 +1,58 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package client
+
+import (
+	"time"
+)
+
+// User represents an n8n cloud user
+type User struct {
+	ID              string    `json:"id"`
+	Email           string    `json:"email"`
+	FirstName       *string   `json:"firstName,omitempty"`
+	LastName        *string   `json:"lastName,omitempty"`
+	IsPending       bool      `json:"isPending"`
+	CreatedAt       time.Time `json:"createdAt"`
+	UpdatedAt       time.Time `json:"updatedAt"`
+	GlobalRole      *Role     `json:"globalRole,omitempty"`
+	InviteAcceptUrl string    `json:"inviteAcceptUrl,omitempty"`
+}
+
+// Role represents a user role in n8n
+type Role struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// CreateUserRequest represents the request to create a new user
+type CreateUserRequest struct {
+	Email     string `json:"email"`
+	Role      string `json:"role,omitempty"`
+	FirstName string `json:"firstName,omitempty"`
+	LastName  string `json:"lastName,omitempty"`
+}
+
+// UpdateUserRoleRequest represents the request to update a user's role
+type UpdateUserRoleRequest struct {
+	NewRoleName string `json:"newRoleName"`
+}
+
+// UsersResponse represents the response from the list users endpoint
+type UsersResponse struct {
+	Data       []User  `json:"data"`
+	NextCursor *string `json:"nextCursor"`
+}
+
+// UserResponse represents the response from single user endpoints
+type UserResponse struct {
+	Data User `json:"data"`
+}
+
+// ErrorResponse represents an error response from the API
+type ErrorResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Hint    string `json:"hint,omitempty"`
+}
